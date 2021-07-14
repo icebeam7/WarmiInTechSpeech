c9075a9d3ca3d78d4ad4820300154e662bf64426
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2021-07-15 01:47:08 +0200

    Add project files.

---
+++ SpeechToTextApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Translation;
+
+namespace SpeechToTextApp
+{
+    class Program
+    {
+        static string key = "";
+        static string region = "";
+        static string idiomaOrigen = "es-MX";
+        static string idiomaTraduccion = "en";
+
+        static SpeechTranslationConfig ConfigurarServicioVoz()
+        {
+            var servicio = SpeechTranslationConfig.FromSubscription(key, region);
+            servicio.SpeechRecognitionLanguage = idiomaOrigen;
+            servicio.AddTargetLanguage(idiomaTraduccion);
+            return servicio;
+        }
+
+        async static Task EscucharMicrofono()
+        {
+            var servicio = ConfigurarServicioVoz();
+            using var microfono = AudioConfig.FromDefaultMicrophoneInput();
+
+            using var reconocedor = new TranslationRecognizer(servicio, microfono);
+
+            Console.WriteLine("Escuchemos lo que tienes que decir:");
+            var resultado = await reconocedor.RecognizeOnceAsync();
+            Console.WriteLine($"HAS DICHO: {resultado.Text}");
+
+            switch (resultado.Reason)
+            {
+                case ResultReason.TranslatedSpeech:
+                    Console.WriteLine($"TRADUCCIÓN: {resultado.Translations[idiomaTraduccion]}");
+                    break;
+                case ResultReason.RecognizedSpeech:
+                    Console.WriteLine("ERROR: El mensaje no pudo ser traducico");
+                    break;
+                default:
+                    Console.WriteLine($"ERROR: {resultado.Reason}");
+                    break;
+            }
+
+            Console.WriteLine("\nPresiona una tecla para salir");
+            Console.ReadKey();
+        }
+
+        async static Task Main(string[] args)
+        {
+            await EscucharMicrofono();
+        }
+    }
+}
---
+++ TextToSpeechApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace TextToSpeechApp
+{
+    class Program
+    {
+        static string key = "";
+        static string region = "";
+        static string idiomaOrigen = "es-MX";
+        static string vozOrigen = "es-MX-DaliaNeural";
+        static string nombreArchivo1 = "mensaje.wav";
+        static string nombreArchivo2 = "conversacion.wav";
+
+        static SpeechConfig ConfigurarServicioVoz()
+        {
+            var servicio = SpeechConfig.FromSubscription(key, region);
+            servicio.SpeechSynthesisLanguage = idiomaOrigen;
+            servicio.SpeechSynthesisVoiceName = vozOrigen;
+            return servicio;
+        }
+
+        static async Task Main()
+        {
+            Console.WriteLine("Escribe un mensaje");
+            var mensaje = Console.ReadLine();
+
+            var servicio = ConfigurarServicioVoz();
+            await GenerarAudio(servicio, mensaje);
+            Console.WriteLine($"Revisa el archivo {nombreArchivo1}");
+            Console.WriteLine("Presiona una tecla para continuar");
+            Console.ReadKey();
+
+            Console.WriteLine("Ahora escucharás una conversación.");
+            await EscucharyGenerarAudio(servicio);
+            Console.WriteLine("Presiona una tecla para finalizar");
+            Console.ReadKey();
+        }
+
+        static async Task GenerarAudio(SpeechConfig servicio, string mensaje)
+        {
+            using var audio = AudioConfig.FromWavFileOutput("audio.wav");
+            using var sintetizador = new SpeechSynthesizer(servicio, audio);
+            await sintetizador.SpeakTextAsync(mensaje);
+        }
+
+        static async Task EscucharyGenerarAudio(SpeechConfig servicio)
+        {
+            using var sintetizador = new SpeechSynthesizer(servicio);
+
+            var ssml = File.ReadAllText("./dialogo.xml");
+            var resultado = await sintetizador.SpeakSsmlAsync(ssml);
+            
+            using var stream = AudioDataStream.FromResult(resultado);
+            await stream.SaveToWaveFileAsync(nombreArchivo2);
+        }
+    }
+}
